0710c95423bb8105f62e175d1403ffc56a1b992f
Essam <mastermail@gmail.com>
2022-05-12 18:37:34 +0200

    Initial commit

---
+++ src/main/java/com/faredo0o/springdatabasesecurity/SpringDatabaseSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.faredo0o.springdatabasesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faredo0o/springdatabasesecurity/SpringDatabaseSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faredo0o.springdatabasesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
